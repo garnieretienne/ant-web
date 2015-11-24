9507f03827031e7fae3bc0bc2407477d015e5a37
Etienne <garnier.etienne@gmail.com>
2015-11-24 15:58:03 +0100

    update the way the sample script receive the API endpoint address

--- scripts/receive_mail.sh
+++ scripts/receive_mail.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-# MTA? receive_message? webhook?
+#
+# Receive a message from STDIN and send it to the REST API at the address
+# sent as a the fisrt parameter
+#
+# E.g. `cat mail.txt | receive_mail.sh http://lists.domain.tld`
 
+entrypoint=$1
 timestamp=$(date +%s)
 cache_file="/tmp/message-${timestamp}"
 while read pipe ; do echo $pipe >> $cache_file ; done
 
 curl -H "Content-Type: text/plain" -X POST \
     --data-urlencode @${cache_file} \
-    http://localhost:3000/api/v1/mailing_lists/receive_message\
+    ${entrypoint}/api/v1/mailing_lists/receive_message \
     &> /dev/null
 exit_status=$?
 
