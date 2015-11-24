90bfcc3d34b622651fe080edd11e38bc5544d1ac
Etienne <garnier.etienne@gmail.com>
2015-11-24 16:51:51 +0100

    update a failed foreign key contraint in postgres

--- db/migrate/20151122122432_add_owner_ref_to_mailing_lists.rb
+++ db/migrate/20151122122432_add_owner_ref_to_mailing_lists.rb
@@ -1,5 +1,6 @@
 class AddOwnerRefToMailingLists < ActiveRecord::Migration
   def change
-    add_reference :mailing_lists, :owner, index: true, foreign_key: true
+    add_reference :mailing_lists, :owner, index: true
+    add_foreign_key :mailing_lists, :users, column: :owner_id
   end
 end
