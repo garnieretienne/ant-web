9701546121631bad6a4a95a025b62ee83e2840b5
Etienne <garnier.etienne@gmail.com>
2015-12-09 19:16:05 +0100

    fix failing tests (disable API token for the test environment)

--- config/initializers/api_token.rb
+++ config/initializers/api_token.rb
@@ -1,2 +1,5 @@
 # Access token for the API
 Rails.application.config.api_token = ENV["API_TOKEN"] || SecureRandom.hex
+
+# Do not use for the test environment
+Rails.application.config.api_token = nil if Rails.env.test?
