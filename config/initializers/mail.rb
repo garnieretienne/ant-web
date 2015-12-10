23b2e7b449af02bb76a51fbe92b7e9e5837566f6
Etienne <garnier.etienne@gmail.com>
2015-12-10 12:38:38 +0100

    add an option to disable TLS cert verification for the SMTP connection through

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -4,19 +4,24 @@ case Rails.env
 when "development"
   Mail.defaults { delivery_method :smtp }
 when "test"
-  Mail.defaults { delivery_method(:test) }
+  Mail.defaults { delivery_method :test }
 end
 
 # Use the SMTP_URL environment variable if exist.
 if ENV["SMTP_URL"]
   smtp_url = URI::parse(ENV["SMTP_URL"] || "smtp://localhost:25")
-  Mail.defaults do
-    delivery_method :smtp, {
-      address: smtp_url.host,
-      port: smtp_url.port,
-      user_name: smtp_url.user,
-      password: smtp_url.password,
-      enable_starttls_auto: smtp_url.scheme == "smtps"
-    }
+
+  smtp_settings = {
+    address: smtp_url.host,
+    port: smtp_url.port,
+    user_name: smtp_url.user,
+    password: smtp_url.password,
+    enable_starttls_auto: smtp_url.scheme == "smtps"
+  }
+
+  if ENV["SMTP_VERIFY_SSL"] == "false"
+    smtp_settings[:openssl_verify_mode] = "none"
   end
+  
+  Mail.defaults { delivery_method :smtp, smtp_settings }
 end
