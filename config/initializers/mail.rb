c1e983d07f25080060876b495294103c434128bd
Etienne <garnier.etienne@gmail.com>
2015-12-10 11:37:25 +0100

    fix the user and password extraction from the SMTP_URL env var

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -10,13 +10,12 @@ end
 # Use the SMTP_URL environment variable if exist.
 if ENV["SMTP_URL"]
   smtp_url = URI::parse(ENV["SMTP_URL"] || "smtp://localhost:25")
-  smtp_user, smtp_password = smtp_url.split(":")
   Mail.defaults do
     delivery_method :smtp, {
       address: smtp_url.host,
       port: smtp_url.port,
-      user_name: smtp_user,
-      password: smtp_password,
+      user_name: smtp_url.user,
+      password: smtp_url.password,
       enable_starttls_auto: smtp_url.scheme == "smtps"
     }
   end
